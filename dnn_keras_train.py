81e259b86ff498da462c74c519093ef2b6946968
tatsuyaokunaga <37996946+tatsuyaokunaga@users.noreply.github.com>
2018-07-29 19:13:30 +0900

    Update dnn_keras_train.py

--- dnn_keras_train.py
+++ dnn_keras_train.py
@@ -3,20 +3,13 @@ import pandas as pd
 from keras.datasets import mnist
 from sklearn.cross_validation import train_test_split
 
-train = pd.read_csv('./input/train.csv')
-test= pd.read_csv('./input/test.csv')
-X = train.drop(["label"], axis=1)
-y = train["label"]
-
+#60,000枚の28x28，10個の数字の白黒画像と10,000枚のテスト用画像データセット．
 (x_train, t_train), (x_test, t_test) = mnist.load_data()
 
 # ラベルデータをカテゴリの1-hotベクトルにエンコードする
-n_features = X.shape[1]
 n_classes = 10
-#訓練データとラベルデータをそれぞれ訓練用、テスト用に分割
-x_train, x_test ,y_train, y_test = train_test_split(X, y, test_size=0.2)
-one_hot_train=keras.utils.to_categorical(y_train , num_classes=10)
-one_hot_test=keras.utils.to_categorical(y_test , num_classes=10)
+one_hot_train=keras.utils.to_categorical(t_train , num_classes)
+one_hot_test=keras.utils.to_categorical(t_test , num_classes)
 
 
 history = model.fit(x_train, one_hot_train, # トレーニングデータ
