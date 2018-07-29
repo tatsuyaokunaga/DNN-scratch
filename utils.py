bba6a33ad36632a985916f23fb3a76049f29e486
tatsuyaokunaga <37996946+tatsuyaokunaga@users.noreply.github.com>
2018-07-29 18:21:05 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,3 +18,7 @@ def cross_entropy_error(y,t, W1, W2,W3,W4, lam):
     batch_size= y.shape[0]
     return -(np.sum(t*np.log(y + 1e-7))+(lam/2)*((W1**2).sum()+(W2**2).sum() +
                                                   (W3**2).sum()+(W4**2).sum()))/batch_size 
+
+# ソフトマックス関数
+def softmax(x):
+    return np.exp(x)/np.sum(np.exp(x), axis=1, keepdims=True)
