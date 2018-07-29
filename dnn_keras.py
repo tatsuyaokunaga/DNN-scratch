1cd19e8cd1cab0d280ede111c5e721a93d169868
tatsuyaokunaga <37996946+tatsuyaokunaga@users.noreply.github.com>
2018-07-29 18:26:27 +0900

    Create dnn_keras.py

---
+++ dnn_keras.py
@@ -0,0 +1,26 @@
+#### ニューラルネットワークの層の構築 ####
+batch_size=64
+epochs= 20
+
+#### 学習プロセスの設定 ####
+# 最初のlayerでは，想定する入力データshapeを784次元に指定
+
+# Sequenceモデル
+model= Sequential()
+model.add(Dense(128,input_shape=(784,)))
+model.add(Activation('tanh'))
+model.add(Dropout(0.25))
+model.add(Dense(256))
+model.add(Activation('tanh'))
+model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+model.add(Dense(256))
+model.add(Activation('tanh'))
+model.add(Dropout(0.25))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+# モデルの学習処理の設定
+#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer='adagrad',
+                           loss = 'categorical_crossentropy',
+                           metrics=['accuracy'])
